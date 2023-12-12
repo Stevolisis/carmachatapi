bab1bdd26c5d27f600cbc75e21c96b5602c93587
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-12 01:46:45 +0100

    get room users done

--- socketServer.js
+++ socketServer.js
@@ -14,15 +14,21 @@ function initializeSocket(server) {
     });
 
     io.use(authSocket).on('connection', (socket) => {
-        socket.emit("me",socket.id);
+        socket.emit("me",socket.decoded.id);
 
         socket.on('join-room',async(args)=>{
             const currentUser = socket.decoded;
-            const room = await Room.findOne({participants:{$all:[args.target,currentUser.id]}});
+            const room = await Room.findOne({participants:{$all:[args.target,currentUser.id]}}).populate('participants');
             console.log('room: ',room);
             if(room){
                 socket.join(room._id);
                 io.to(room._id).emit('chats',room.chats);
+                io.to(room._id).emit('room_users',room.participants);
+
+                // socket.on('group-chat',(msg)=>{
+                //     io.to(user.room).emit('message', formatMessage(user.id, user.userName, msg));
+                // });
+
             }else{
                 const room = await registerRoom([args.target,currentUser.id]);
                 console.log('room22222: ',room);
@@ -30,6 +36,7 @@ function initializeSocket(server) {
                 if(room){
                     socket.join(room._id);
                     io.to(room._id).emit('chats',room.chats);
+                    io.to(room._id).emit('room_users',room.participants);
                 }
             }
         });
