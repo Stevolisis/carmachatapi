c6f82e1fc60d9a66f9a1c3076c5f5ea5cce6e13a
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-14 09:07:11 +0100

    me emit brought into groupchat on and it worked

--- handlers/handleGroupChat.js
+++ handlers/handleGroupChat.js
@@ -4,22 +4,21 @@ async function handleGroupChat(io,room, user, msg) {
     const time = new Date().toISOString();
     const saveNewMsg = await addChat(room,user,msg,time);
     
-    // if(saveNewMsg){
+    if(saveNewMsg){
         io.to(room._id.toString()).emit('message', {
             uid:user._id,
             userName:user.full_name,
             text:msg,
             time:time
        }); 
-       console.log('zzzzzzzzzzzzzzz',msg);
-    // }else{
-    //     io.to(room._id).emit('message', {
-    //         uid:user._id,
-    //         userName:user.full_name,
-    //         text:" ",
-    //         time:time
-    //     }); 
-    // }
+    }else{
+        io.to(room._id).emit('message', {
+            uid:user._id,
+            userName:user.full_name,
+            text:" ",
+            time:time
+        }); 
+    }
 }
 
 module.exports = { handleGroupChat };
--- handlers/handleSocketConnection.js
+++ handlers/handleSocketConnection.js
@@ -5,19 +5,18 @@ const { handleGroupChat } = require('./handleGroupChat');
 
 async function handleSocketConnection(socket, io) {
     const currentUser = socket.decoded;
-    socket.emit("me",currentUser.id);
 
     socket.on('join-room',async(args)=>{
         const room = await findOrCreateRoom(args.target, currentUser.id);
 
         if (room) {
             socket.join(room._id.toString());
+            socket.emit("me",currentUser.id);
             io.to(room._id.toString()).emit('chats', room.chats);
             io.to(room._id.toString()).emit('room_users', room.participants);
 
             socket.on('group-chat',(msg)=>{
                 const user = room.participants.find(user=>user._id == currentUser.id);
-                console.log('qqqqqqqqqqqqqqqqq',user);
                 handleGroupChat(io,room,user,msg);
             });
         }
