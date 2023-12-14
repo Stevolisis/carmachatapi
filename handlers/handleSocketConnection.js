90dc88c85c8b32baa1aab484764cb9d985179221
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-14 08:49:11 +0100

    join room error fixed by adding .toString() to room._id to convert t string for socket to join room

--- handlers/handleSocketConnection.js
+++ handlers/handleSocketConnection.js
@@ -11,9 +11,9 @@ async function handleSocketConnection(socket, io) {
         const room = await findOrCreateRoom(args.target, currentUser.id);
 
         if (room) {
-            socket.join(room._id);
-            io.to(room._id).emit('chats', room.chats);
-            io.to(room._id).emit('room_users', room.participants);
+            socket.join(room._id.toString());
+            io.to(room._id.toString()).emit('chats', room.chats);
+            io.to(room._id.toString()).emit('room_users', room.participants);
 
             socket.on('group-chat',(msg)=>{
                 const user = room.participants.find(user=>user._id == currentUser.id);
